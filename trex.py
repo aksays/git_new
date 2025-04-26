4c343cd5057ca818cfece5d84250485c5f39a907
aks <aksay.sriram@agiratech.com>
2025-04-26 17:45:06 +0530

    hey corrected this flow ever

--- trex.py
+++ trex.py
@@ -2,8 +2,8 @@ import requests
 test_list = [i for i in range(1,100)]
 print('list')
 sdds = [i for i in range(10,100)]
-print(sdds, "------", test_list)
+print( "------", test_list)
 test = requests.get("https://www.example.com")
 print(test.status_code)
 print("Sadhmaster sivandha kangal siripu konda idayam")
-print ("why missed bro - I don't ducking care, what's going on here")
+print ("Hey there is a mistake here, I will correct this - I don't ducking care, what's going on here")
