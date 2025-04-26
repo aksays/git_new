5ae8442c49c8ae9b20301dbc3c10f55ad85ee5ed
aks <aksay.sriram@agiratech.com>
2025-04-26 17:30:01 +0530

    commit from document

--- trex.py
+++ trex.py
@@ -1,5 +1,7 @@
 import requests
+test_list = [i for i in range(1,100)]
+print('list')
 
 test = requests.get("https://www.example.com")
 print(test.status_code)
-print("world hai")
+print("Sadhmaster sabeski")
