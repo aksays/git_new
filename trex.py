8cb54004372baf5131a07619188268ebdd9ec464
aks <aksay.sriram@agiratech.com>
2025-04-26 17:40:39 +0530

    from download to your reply

--- trex.py
+++ trex.py
@@ -1,9 +1,9 @@
 import requests
 test_list = [i for i in range(1,100)]
 print('list')
-[i for i in range(10,100)]
-
+sdds = [i for i in range(10,100)]
+print(sdds, "------", test_list)
 test = requests.get("https://www.example.com")
 print(test.status_code)
 print("Sadhmaster sivandha kangal siripu konda idayam")
-print ("why missed bro")
+print ("why missed bro - I don't ducking care, what's going on here")
