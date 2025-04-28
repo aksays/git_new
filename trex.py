87bc86494f6000d121e78ac7c72e958923f9f73b
aks <aksay.sriram@agiratech.com>
2025-04-28 16:51:53 +0530

    new branch push

--- trex.py
+++ trex.py
@@ -2,9 +2,9 @@ import requests
 test_list = [i for i in range(1,100)]
 print("anbeSadasivam adhuve manam")
 
-sdds = [i for i in range(10,100)]
+
 print( "------", test_list)
 test = requests.get("https://www.example.com")
 print(test.status_code)
 print("Sadhmaster sivandha kangal siripu konda idayam")
-print ("I have a diff idea, lets remvoe these codes ")
+print("manusana ivanunga")
